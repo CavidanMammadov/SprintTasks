d5b0d07a861acc2fecd69cc27a9924bb6ef911ac
CavidanMammadov <cavidanbm-bp215@code.edu.az>
2025-04-21 21:04:03 +0400

    added all tasks

---
+++ sprint2a/sprint2a/sprint2a.py
@@ -0,0 +1,57 @@
+﻿#1) 5, 10, 15, 20 rəqəmlərindən ibarət “rəqəmlər” adlı list/vector yaradın
+# rəqəmlər = [5, 10, 15, 20]
+#2) “rəqəmlər” listinin/vectorunun uzunluğunu tapın
+# print(len(rəqəmlər))
+
+#3) “rəqəmlər” listinə/vectoruna 25 elementini əlavə edin
+# rəqəmlər.append(25)
+
+#4) “rəqəmlər” listinin/vectorunun 2-ci indeksinə 12 elementini əlavə edin
+# rəqəmlər.insert(2, 12)
+
+#5) 1, 2, 3 və 4, 5, 6 listlərini/vectorlarını birləşdirin
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+
+# result = list1 + list2
+
+
+#6) “rəqəmlər” listindən/vectorundan 2-ci və 3-cü elementləri seçin
+#print(rəqəmlər[2:4])
+
+#7) “rəqəmlər” listinin/vectorunun ilk elementini 50 ilə əvəz edin 
+#rəqəmlər[0] = 50
+
+
+#8) “rəqəmlər” listində/vectorunda 19 elementinin olub-olmadığını yoxlayın
+#print(19 in rəqəmlər)
+
+#9) "a", "b", "a", "c" listində/vectorunda "a" elementinin neçə dəfə təkrarlandığını tapın
+# list = ["a", "b", "a", "c"]
+
+# print(list.count("a"))
+  
+
+#10) "x", "y", "x", "z" listindən/vectorundan "x" elementlərini silin
+# list = ["x", "y", "x", "z"]
+
+# while "x" in list:
+#     list.remove("x")
+
+
+#11) 7, 2, 9, 1 listini/vectorunu azalan sırayla sıralayın
+# list = [7, 2, 9, 1]
+
+# list.sort(reverse=True)
+
+
+#12) “rəqəmlər” listindən/vectorundan 10-dan böyük elementləri seçin
+# böyük_elements = [x for x in rəqəmlər if x > 10]
+
+
+
+
+
+
+
+
---
+++ sprint2b/sprint2b/sprint2b.py
@@ -0,0 +1,25 @@
+﻿import pandas as pd
+#1) 10, 20, 30, 40 elementlərindən ibarət s1 adlı series/vector yaradın
+
+s1 = pd.Series([10, 20, 30, 40])
+
+s1.index = ['w', 'x', 'y', 'z']
+
+#3) Python: {'p': 5, 'q': 10, 'r': 15} dictionary-dən s2 adlı Series yaradın
+   # R: list(p = 5, q = 10, r = 15) listindən v2 adlı named vektor yaradın
+
+
+s2 = pd.Series({'p': 5, 'q': 10, 'r': 15})
+print(s2['q'])
+print(s1[s1 > 25])
+print(s1[s1 > 20] / 10)
+
+
+
+df1 = pd.DataFrame([(1, 2), (3, 4)])
+df1.index = ['r1', 'r2']
+df1.columns = ['c1', 'c2']
+df2 = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+print(df2[df2['A'] > 1])
+
+

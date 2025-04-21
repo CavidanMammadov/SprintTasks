0cf10003e34ce266fed3f971a7b02e03c8f7493b
CavidanMammadov <cavidanbm-bp215@code.edu.az>
2025-04-21 19:55:29 +0400

    Added sprint1

---
+++ HomeWork/HomeWork/HomeWork.py
@@ -0,0 +1,89 @@
+﻿from errno import EADDRINUSE
+import string
+# 1)ededi stirnge cevirmek
+
+# eded = 123
+# string_value= str(eded)
+# print(string_value)         
+# print(type(string_value))
+
+
+
+
+# ededi chara cevirmek
+
+
+# eded =123
+# char_value = chr(123)
+# print(char_value)         
+# print(type(char_value))
+
+
+
+# 2)19.99 dəyərini tam ədədə çevirin və nəticəni çap edin.
+# eded = 19.99
+# tam_qiymet = int(eded)
+# print(tam_qiymet)
+
+
+#3) "500" dəyərini numericə çevirin və 2-yə bölüb nəticəni çap edin.
+# value = "500"
+# numeric_value = int(value)    
+# result = numeric_value / 2
+
+# print(result)  
+
+
+#4) a = 8 və b = 12 yaradın. a < b və a == b şərtlərini yoxlayın, nəticələri çap edin.
+# a = 8
+# b = 12
+
+# print("a < b:", a < b)     
+# print("a == b:", a == b)  
+
+# 5)x = 5, y = 10, z = 15 yaradın. (x < y) and (y < z) şərtini yoxlayın və nəticəni çap edin.
+# x = 5
+# y = 10
+# z = 15
+
+# result = (x < y) and (y < z)
+# print(result)
+
+
+
+#6) 25-i 4-ə bölün. Tam bölmə, qalıq və adi bölmə nəticələrini çap edin.
+# a = 25
+# b = 4
+
+# print(a // b)
+# print(a % b)
+# print(a / b)
+
+#7) 3-ü 4-cü dərəcəyə qaldırın və nəticəni çap edin.
+# result = 3 ** 4
+# print(result)
+
+#8) qiymet = 75.5 yaradın. Onu tam ədədə çevirin və tipini yoxlayın.
+# qiymet = 75.5
+# tam = int(qiymet)
+# print(tam)
+# print(type(tam))
+
+
+
+#9) n = 20 yaradın. (n > 10) or (n < 5) və (n > 15) and (n < 25) şərtlərini yoxlayın, nəticələri çap edin.
+# n = 20
+
+# print((n > 10) or (n < 5))
+# print((n > 15) and (n < 25))
+
+#10) "42.8" dəyərini əvvəl float-a, sonra tam ədədə çevirin və hər addımda tipi yoxlayın.
+# value = "42.8"
+
+# float_value = float(value)
+# print(float_value)
+# print(type(float_value))
+
+# int_value = int(float_value)
+# print(int_value)
+# print(type(int_value))
